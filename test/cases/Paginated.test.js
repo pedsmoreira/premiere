759c6cff0ef73a3d39dafbfff91e8a55944cc6c5
Pedro Moreira <pedsmoreira@gmail.com>
2018-08-12 17:00:26 -0300

    Add paginated test

---
+++ test/cases/Paginated.test.js
@@ -0,0 +1,38 @@
+import MockAdapter from 'axios-mock-adapter';
+import Model from '../../src/Model';
+
+describe('PaginagedAll', () => {
+  class User extends Model {
+    static basename = 'user';
+
+    static async paginated(page) {
+      const { data } = await this.api.http.get(`${this.pluralPath}?page=${page}`);
+      return this.makeArray(data);
+    }
+
+    normalizeName(name) {
+      return name.toLowerCase();
+    }
+  }
+
+  const axiosAdapter = new MockAdapter(User.api.http);
+
+  afterEach(() => {
+    axiosAdapter.reset();
+  });
+
+  it('finds all models', async () => {
+    axiosAdapter.onGet('users?page=1').reply(200, [{ id: 1, name: 'John Doe' }, { id: 2, name: 'Jane Doe' }]);
+    axiosAdapter.onGet('users?page=2').reply(200, [{ id: 3, name: 'Bob Vence' }, { id: 2, name: 'Refrigeration' }]);
+
+    const firstPageUsers = await User.paginated(1);
+    expect(firstPageUsers.length).toEqual(2);
+    expect(firstPageUsers[0]).toBeInstanceOf(User);
+    expect(firstPageUsers).toEqual([{ id: 1, name: 'john doe' }, { id: 2, name: 'jane doe' }]);
+
+    const secondPageUsers = await User.paginated(2);
+    expect(secondPageUsers.length).toEqual(2);
+    expect(secondPageUsers[0]).toBeInstanceOf(User);
+    expect(secondPageUsers).toEqual([{ id: 3, name: 'bob vence' }, { id: 2, name: 'refrigeration' }]);
+  });
+});
