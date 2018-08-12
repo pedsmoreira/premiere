90a885c652f5b3e0368d88b4992b2a8efb1ea330
Pedro Moreira <pedsmoreira@gmail.com>
2018-08-12 17:06:22 -0300

    Update paginated test

--- test/cases/Paginated.test.js
+++ test/cases/Paginated.test.js
@@ -5,9 +5,10 @@ describe('PaginagedAll', () => {
   class User extends Model {
     static basename = 'user';
 
-    static async paginated(page) {
+    static async paginated(page, metaCallback: Function) {
       const { data } = await this.api.http.get(`${this.pluralPath}?page=${page}`);
-      return this.makeArray(data);
+      metaCallback(data.meta);
+      return this.makeArray(data.users);
     }
 
     normalizeName(name) {
@@ -22,17 +23,28 @@ describe('PaginagedAll', () => {
   });
 
   it('finds all models', async () => {
-    axiosAdapter.onGet('users?page=1').reply(200, [{ id: 1, name: 'John Doe' }, { id: 2, name: 'Jane Doe' }]);
-    axiosAdapter.onGet('users?page=2').reply(200, [{ id: 3, name: 'Bob Vence' }, { id: 2, name: 'Refrigeration' }]);
+    axiosAdapter.onGet('users?page=1').reply(200, {
+      users: [{ id: 1, name: 'John Doe' }, { id: 2, name: 'Jane Doe' }],
+      meta: { current_page: 1, total_pages: 2 }
+    });
 
-    const firstPageUsers = await User.paginated(1);
+    axiosAdapter.onGet('users?page=2').reply(200, {
+      users: [{ id: 3, name: 'Bob Vence' }, { id: 2, name: 'Vence Refrigeration' }],
+      meta: { current_page: 2, total_pages: 2 }
+    });
+
+    const metaFn = jest.fn();
+
+    const firstPageUsers = await User.paginated(1, metaFn);
     expect(firstPageUsers.length).toEqual(2);
     expect(firstPageUsers[0]).toBeInstanceOf(User);
     expect(firstPageUsers).toEqual([{ id: 1, name: 'john doe' }, { id: 2, name: 'jane doe' }]);
+    expect(metaFn).toHaveBeenCalledWith({ current_page: 1, total_pages: 2 });
 
-    const secondPageUsers = await User.paginated(2);
+    const secondPageUsers = await User.paginated(2, metaFn);
     expect(secondPageUsers.length).toEqual(2);
     expect(secondPageUsers[0]).toBeInstanceOf(User);
-    expect(secondPageUsers).toEqual([{ id: 3, name: 'bob vence' }, { id: 2, name: 'refrigeration' }]);
+    expect(secondPageUsers).toEqual([{ id: 3, name: 'bob vence' }, { id: 2, name: 'vence refrigeration' }]);
+    expect(metaFn).toHaveBeenCalledWith({ current_page: 2, total_pages: 2 });
   });
 });
